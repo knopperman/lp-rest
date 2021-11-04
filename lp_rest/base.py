3653c4a905b069845203fb92b9ca4469ea8c0abc
knopperman <knopperman@users.noreply.github.com>
2021-11-04 10:30:11 +0000

    ✅ Ready to clone and code.

--- lp_rest/base.py
+++ lp_rest/base.py
@@ -1,7 +1,7 @@
 """
-project_name base module.
+lp_rest base module.
 
-This is the principal module of the project_name project.
+This is the principal module of the lp_rest project.
 here you put your main classes and objects.
 
 Be creative! do whatever you want!
